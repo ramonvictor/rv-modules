925110f8818edb28b246756e8b396e694223e4c5
Ramon Bezerra <ramon.bezerra@booking.com>
2016-06-13 23:06:55 +0200

    change error handling testing to assert.throws api

--- test/index.js
+++ test/index.js
@@ -2,20 +2,15 @@ const t = require('assert');
 const RV = require('../src/main.js');
 
 test('define: name is invalid', () => {
-	try {
+	t.throws(() => {
 		RV.define();
-	} catch(e) {
-		t.equal(e.message, 'define: module name is missing or is not a string');
-	}
+	}, 'define: module name is missing or is not a string');
 });
 
-
 test('define: callback is invalid', () => {
-	try {
+	t.throws(() => {
 		RV.define('my-module');
-	} catch(e) {
-		t.equal(e.message, 'define: callback is undefined or is not a function');
-	}
+	}, 'define: callback is undefined or is not a function');
 });
 
 test('define: avoid module override', () => {
@@ -31,11 +26,9 @@ test('define: avoid module override', () => {
 });
 
 test('require: undefined module', () => {
-	try {
+	t.throws(() => {
 		RV.require('my-module-5');
-	} catch(e) {
-		t.equal(e.message, 'require: module `my-module-5` is not defined');
-	}
+	}, 'require: module `my-module-5` is not defined');
 });
 
 test('require: log return', () => {
